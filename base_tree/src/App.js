90ad41e92518b64e08fa7039d21c1375e430fa37
Andrii Baldyniuk <baldynuk.andrey87@gmail.com>
2021-01-11 02:26:23 +0200

    home task lesson 8

--- base_tree/src/App.js
+++ base_tree/src/App.js
@@ -1,90 +1,28 @@
-
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from 'react-redux';
+
+
 export default function App() {
-    const [count, setCounter] = useState(1);  
-    const [users, setUser] = useState();
-    let [posts, setPost] = useState();
-    let [comments, setComment] = useState();
-
-    const chooseItemUser = (e)=>{
-        e.preventDefault();
-        setUser();
-    }
-
-    let chooseItemPost = (e)=>{
-        e.preventDefault();
-        setPost();
-    }
-
-    let chooseItemComment = (e)=>{
-        e.preventDefault();
-        setComment();
-    }
-
-    let counterInc = (e)=>{
-        e.preventDefault();
-    setCounter(currentValue=>currentValue<10 ? currentValue+1:currentValue);
-    }
-    let counterDec = (e)=>{
-        e.preventDefault();
-        setCounter(currentValue=>currentValue>1 ? currentValue-1:currentValue);
-    }
-
-    const cleanCounter = () => setCounter(1);
-
+    const counter = useSelector(({counter}) => counter)
+    const user = useSelector(({user}) => user)
+    const dispatch = useDispatch();
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${count}`)
-            .then(value => value.json())
-            .then(value => setUser(value))
-            
-    }, [count])
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${count}`)
-            .then(value => value.json())
-            .then(value => setPost(value))
-    }, [count])
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/comments/${count}`)
-            .then(value => value.json())
-            .then(value => setComment(value))
-    }, [count])
- let myMaxValue='';
+        if(counter > 0 && counter < 10){
+            fetch(`https://jsonplaceholder.typicode.com/users/${counter}`)
+                .then(value => value.json())
+                .then(value => dispatch({type: 'SET_USER', payload: value}))
+        }
+
+    })
     return (
         <div>
-            <div>
-            <button onClick={chooseItemUser} value={users} onChange={({ target: { value } }) => setUser(value)}>exclude User</button>
-            </div> 
-            <div>
-            <button onClick={chooseItemPost} value={posts} onChange={({ target: { value } }) => setPost(value)}>exclude Post</button>
-            </div>
-            <div>
-            <button onClick={chooseItemComment} value={comments} onChange={({ target: { value } }) => setComment(value)}>exclude Comment</button>
-            </div> 
-            <form> 
-            <button onClick={counterInc}>+</button>
-            <input type="number" min={1} max={myMaxValue} value={count} onChange={({ target: { value } }) => setCounter(value)} />
-            <button onClick={counterDec}>-</button>
-            <button onClick={cleanCounter}>reset</button>
-            </form>
-            {!!users &&
-            (<div>USER:
-                <p>{users.id} - {users.name} - {users.username} </p>
-            </div>)
-            }
-            {!!posts &&
-            (<div>POST:
-                <p>{posts.id} - {posts.title} </p>
-            </div>)
-            }
-            {!!comments &&
-            (<div>COMMENT:
-                <p>{comments.id} - {comments.name} - {comments.body} </p>
-            </div>)
-            }
+            counter - {counter}
+            <br/>
+            <button onClick={() => dispatch({type: 'INC_COUNTER'})}>inc</button>
+            <button onClick={() => dispatch({type: 'DEC_COUNTER'})}>dec</button>
+            <button onClick={() => dispatch({type: 'RESET'})}>reset</button>
+            {user && <div>{user.name} - {user.username} - {user.email}</div>}
         </div>
-        
-    )}
-
-
-
+    );
+}
